f90447c052bfa084374f7acd9ebd77e947388894
Martin Hagstrom <marhag87@gmail.com>
2016-04-15 15:41:25 +0200

    Style changes to comply with pylint

--- rebeccablackbot.py
+++ rebeccablackbot.py
@@ -1,45 +1,57 @@
 #!/bin/env python
-import discord
+# -*- coding: utf-8 -*-
+"""A bot for discord that links videos and prints topic changes"""
 import asyncio
+import sys
 from datetime import datetime
 import yaml
-import sys
+import discord
 
-configfile = 'config.yaml'
-try:
-  with open(configfile, 'r') as ymlfile:
+def load_config(configfile):
+    """Return a dict with configuration from the supplied yaml file."""
     try:
-      cfg = yaml.load(ymlfile)
-    except yaml.parser.ParserError:
-      print('Could not parse config file: %s' % configfile)
-      sys.exit(1)
-except IOError:
-  print('Could not open config file: %s' % configfile)
-  sys.exit(1)
+        with open(configfile, 'r') as ymlfile:
+            try:
+                config = yaml.load(ymlfile)
+            except yaml.parser.ParserError:
+                print('Could not parse config file: %s' % configfile)
+                sys.exit(1)
+    except IOError:
+        print('Could not open config file: %s' % configfile)
+        sys.exit(1)
+    return config
 
-client = discord.Client()
+CLIENT = discord.Client()
 
-@client.event
+@CLIENT.event
 @asyncio.coroutine
 def on_ready():
-  print('Logged in as')
-  print(client.user.name)
-  print(client.user.id)
-  print('------')
+    """Print user information once logged in"""
+    print('Logged in as')
+    print(CLIENT.user.name)
+    print(CLIENT.user.id)
+    print('------')
 
-@client.event
+@CLIENT.event
 @asyncio.coroutine
 def on_message(message):
-  if message.content.startswith('!friday'):
-    if datetime.today().weekday() == 4:
-      yield from client.send_message(message.channel, 'https://www.youtube.com/watch?v=kfVsfOSbJY0')
-    else:
-      yield from client.send_message(message.channel, 'It is not Friday. Let me link you a video that can educate you on the matter: https://www.youtube.com/watch?v=kfVsfOSbJY0')
+    """Handle on_message event"""
+    if message.content.startswith('!friday'):
+        if datetime.today().weekday() == 4:
+            yield from CLIENT.send_message(message.channel,
+                                           'https://www.youtube.com/watch?v=kfVsfOSbJY0')
+        else:
+            yield from CLIENT.send_message(message.channel,
+                                           'It is not Friday. Let me link you a video that ' +
+                                           'can educate you on the matter: ' +
+                                           'https://www.youtube.com/watch?v=kfVsfOSbJY0')
 
-@client.event
+@CLIENT.event
 @asyncio.coroutine
 def on_channel_update(before, after):
-  if before.topic != after.topic:
-    yield from client.send_message(after, 'New topic:\n```\n' + after.topic + '```')
+    """Handle on_channel_update event"""
+    if before.topic != after.topic:
+        yield from CLIENT.send_message(after, 'New topic:\n```\n' + after.topic + '```')
 
-client.run(cfg.get('token'))
+CFG = load_config('config.yaml')
+CLIENT.run(CFG.get('token'))

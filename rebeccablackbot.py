9c00e0ebe38ce4774b769ed59f08bf0804c5feda
Martin Hagstrom <marhag87@gmail.com>
2016-04-16 11:19:54 +0200

    Use pyyamlconfig to load config file

--- rebeccablackbot.py
+++ rebeccablackbot.py
@@ -4,22 +4,8 @@
 import asyncio
 import sys
 from datetime import datetime
-import yaml
+from pyyamlconfig import load_config
 import discord
-
-def load_config(configfile):
-    """Return a dict with configuration from the supplied yaml file."""
-    try:
-        with open(configfile, 'r') as ymlfile:
-            try:
-                config = yaml.load(ymlfile)
-            except yaml.parser.ParserError:
-                print('Could not parse config file: %s' % configfile)
-                sys.exit(1)
-    except IOError:
-        print('Could not open config file: %s' % configfile)
-        sys.exit(1)
-    return config
 
 CLIENT = discord.Client()
 
